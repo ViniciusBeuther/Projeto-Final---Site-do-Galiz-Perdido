5f85efd717c87bb8b81bd4725e6e198a923f8935
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-05-30 21:39:55 -0300

    New: Cards / Positions

--- Galize_Perdido/src/Componets/Header.jsx
+++ Galize_Perdido/src/Componets/Header.jsx
@@ -2,8 +2,8 @@ import Logo from "../../public/galizé.png"
 
 const Header = () => {
     return(
-        <header id="header" className="flex items-center justify-center gap-12 text-[36px] text-customGreen-500 font-bold">
-            <h2>GALIZÉ</h2>
+        <header id="header" className="flex items-center justify-center gap-12 text-customGreen-500 font-bold">
+            <h2 className="lg:text-[36px] md:text-[24px]">GALIZÉ</h2>
             <img src={Logo} alt="Logo" className="w-[128px] h-[128px]" />
             <h2>PERDIDO</h2>
         </header>
