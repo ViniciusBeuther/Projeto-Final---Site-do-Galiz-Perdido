5b4ea257b8f535ad17e2254cada5b5409e180f59
ViniciusBeuther <vinicius.beuther15@gmail.com>
2024-06-18 13:06:25 -0300

    Upd: Fetch animal_address

--- Galize_Perdido/src/Classes/Card.js
+++ Galize_Perdido/src/Classes/Card.js
@@ -1,52 +1,53 @@
 import dayjs from "dayjs";
 import supabase from "../supabase/supabase";
 
-class Card{
-    constructor( animalType, locationID, time, image ){
-        this.animalType = animalType
+class Card {
+    constructor(animalType, locationID, time, image) {
+        this.animalType = animalType;
         this.locationID = locationID;
-        this.locationID = null;
-        this.getLocation( location )
-        this.time = dayjs(time)
-        this.image = image
+        this.location = null;
+        this.time = dayjs(time);
+        this.image = image;
+        this.getLocation(locationID);
     }
 
-    // Return the date/hour in DD/MM/YYYY HH:MM 
-    formattedDate(){
+    formattedDate() {
         const day = this.time.date();
         const month = this.time.month() + 1;
         const year = this.time.year();
         const hour = this.time.hour();
-        const minute = this.time.minute(); 
+        const minute = this.time.minute();
 
-        //Formatting to MM/DD/YYYY
-        const formattedDay = String( day ).padStart( 2,0 );
-        const formattedMonth = String( month ).padStart( 2,0 );
-        const formattedYear = String( year ).padStart( 2,0 );
-        const formattedHour = String( hour ).padStart( 2,0 );
-        const formattedMinute = String( minute ).padStart( 2,0 );
+        const formattedDay = String(day).padStart(2, '0');
+        const formattedMonth = String(month).padStart(2, '0');
+        const formattedYear = String(year).padStart(2, '0');
+        const formattedHour = String(hour).padStart(2, '0');
+        const formattedMinute = String(minute).padStart(2, '0');
 
-
-        const fulldate = `${ formattedDay }/${ formattedMonth }/${ formattedYear } ${ formattedHour }:${ formattedMinute }`;
-
-        return fulldate; 
-
+        const fulldate = `${formattedDay}/${formattedMonth}/${formattedYear} ${formattedHour}:${formattedMinute}`;
+        return fulldate;
     }
 
-    async getLocation(){
-        try{
+    async getLocation(locationID) {
+        try {
             let { data: animals_address, error } = await supabase
-            .from('animals_address')
-            .select('*') 
-            .eq("id", this.locationID);
-            this.location = animals_address;
-            return console.log("Sucesso....")     
-        } catch( error ){
-            return console.log("Algo deu errado...", error)
+                .from('animals_address')
+                .select('*')
+                .eq('id', locationID);
+
+            if (error) throw error;
+
+            if (animals_address.length > 0) {
+                this.location = animals_address[0].address;
+                console.log(this)
+            } else {
+                this.location = "Unknown Location";
+            }
+            console.log('Sucesso....');
+        } catch (error) {
+            console.log('Algo deu errado...', error);
         }
     }
-
-    
 }
 
-export default Card
+export default Card;
--- Galize_Perdido/src/Componets/MainBackground.jsx
+++ Galize_Perdido/src/Componets/MainBackground.jsx
@@ -1,32 +1,42 @@
+import React, { useEffect, useState } from 'react';
 import Card from "../Classes/Card";
 import Filter from "../Classes/Filter";
 import AnimalCard from "./Card";
 import FilterBar from "./FilterBar";
 
-
 const MainBackground = (props) => {
   const data = props.data;
   const filter = new Filter();
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const loadedCards = await Promise.all(
+        data.map(async (animal) => {
+          const card = new Card(
+            animal.animal_type,
+            animal.address,
+            animal.time,
+            animal.image
+          );
+          await card.getLocation(animal.address);
+          return card;
+        })
+      );
+      setCards(loadedCards);
+    };
+
+    fetchData();
+  }, [data]);
 
   return (
     <section className="flex flex-col items-center" id="main_body">
       <FilterBar />
       <div>
         <article className="flex flex-wrap p-10 max-w-[1440px] items-start justify-center gap-5">
-            
-          {data.map((animal, idx) => {
-            const card = new Card(
-              animal.animal_type,
-              animal.address,
-              animal.time,
-              animal.image
-            );
-            console.log(card)
-            return (
-              
-              <AnimalCard card={card} key={idx} />
-            );
-          })}
+          {cards.map((card, idx) => (
+            <AnimalCard card={card} key={idx} />
+          ))}
         </article>
       </div>
     </section>
